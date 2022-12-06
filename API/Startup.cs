e31ea7a8c31bb05f8e30fe53872a803f435a8dff
rithwt <rithwt@hotmail.com>
2022-12-06 16:41:06 +0200

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,23 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
-using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
 using API.Middleware;
-using API.Errors;
+using API.Extensions;
 
 namespace API
 {
@@ -37,42 +23,21 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
+
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddDbContext<StoreContext>(x=>x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
 
-            services.Configure<ApiBehaviorOptions>(options=>
-            {
-                options.InvalidModelStateResponseFactory = actionContext=>
-                {
-                    var errors=actionContext.ModelState
-                        .Where(e=>e.Value.Errors.Count>0)
-                        .SelectMany(x=>x.Value.Errors)
-                        .Select(x=>x.ErrorMessage).ToArray();
-                    
-                    var errorResponse=new ApiValidationErrorResponse
-                    {
-                        Errors=errors
-                    };
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            if (env.IsDevelopment())
-            {                
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
+            
+            app.UseSwaggerDocumentation();
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
