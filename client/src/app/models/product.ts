2cb317687832e5853d86bbbf2499f0569c1175d7
rithwt <rithwt@hotmail.com>
2023-02-13 14:31:54 +0200

    end of section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,15 @@
+
+    export interface Product {
+        id: number;
+        name: string;
+        description: string;
+        price: number;
+        pictureUrl: string;
+        productType: string;
+        productBrand: string;
+    }
+
+
+  
+
+
